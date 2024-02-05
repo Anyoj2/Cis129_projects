734e6b080a41ef3a749c4720e10936bed216dc6f
Anyoj2 <159051619+Anyoj2@users.noreply.github.com>
2024-02-05 12:57:02 -0700

    Update Cis129_lab02_Shortstory.py

--- Cis129_lab02_Shortstory.py
+++ Cis129_lab02_Shortstory.py
@@ -3,5 +3,5 @@
 #Prints of a short story
 print('Hello again, this time not the world but the people from earth')
 print('I, Allen the Alien, presents himself to warn you from the dangers of the viltrum empire...')
-print('should have used more prints for the last sentece')
+print('Should have used more prints for the last sentece')
 print('The end')
