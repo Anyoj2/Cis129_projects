70821c2fa32dba21f30fd99a998644ec9b8d0290
Anyoj2 <159051619+Anyoj2@users.noreply.github.com>
2024-02-18 15:00:25 -0700

    Update Part 3.2.py

--- Part 3.2.py
+++ Part 3.2.py
@@ -1,6 +1,6 @@
 #Angel Solis
-#Cis129
+#Cis129 Module 4 Assignment
 #Exercise 3.2
-a = b = 7               #No problems. Works as expected
+a = b = 7
 print('a =', a, '\nb =', b)
-
+#No problems. Works as expected
